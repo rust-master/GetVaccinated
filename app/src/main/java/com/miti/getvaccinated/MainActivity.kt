79a84ef457ecbcfe412813ac82c47050601922d3
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-05-12 17:39:07 +0500

    google sign in code add

--- app/src/main/java/com/miti/getvaccinated/MainActivity.kt
+++ app/src/main/java/com/miti/getvaccinated/MainActivity.kt
@@ -8,24 +8,141 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.miti.getvaccinated.ui.theme.GetVaccinatedTheme
+import com.google.firebase.auth.FirebaseAuth
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import android.content.Intent
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+    private val RC_SIGN_IN = 123
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mAuth = FirebaseAuth.getInstance();
+        createRequest();
 
         setContent {
             GetVaccinatedTheme {
                 Surface(color = Color.White) {
-                    App()
+                    App(mGoogleSignInClient,RC_SIGN_IN,this)
                 }
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "G E T  V A C C I N A T E D", Toast.LENGTH_SHORT).show()
+        val user = mAuth!!.currentUser
+        if (user != null) {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun createRequest() {
+        val default_web_client_id = "651368021412-evv086567h9qfdj2f1ac9n5io1l2b845.apps.googleusercontent.com"
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(default_web_client_id)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = mAuth!!.currentUser
+                    val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@MainActivity, "Sorry auth failed!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+    }
+
+}
+fun signIn(mGoogleSignInClient: GoogleSignInClient?, RC_SIGN_IN: Int,mainActivity: MainActivity) {
+    val signInIntent = mGoogleSignInClient!!.signInIntent
+    mainActivity.startActivityForResult(signInIntent, RC_SIGN_IN)
 }
 
 @Composable
-fun App() {
+fun App(mGoogleSignInClient: GoogleSignInClient?, RC_SIGN_IN: Int, mainActivity: MainActivity) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+            Card(
+                modifier = Modifier
+                    .height(90.dp)
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 5.dp, top = 10.dp, bottom = 10.dp)
+                    .clickable {
+                        signIn(mGoogleSignInClient, RC_SIGN_IN , mainActivity);
+                    },
+                backgroundColor = Color.Red,
+                shape = RoundedCornerShape(10.dp)
 
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                    Text(
+                        text = "Google Sign In",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+            }
+    }
 }
