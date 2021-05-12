f5b663e52aacab1d78ad134129c70558ea47a8aa
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-05-12 17:55:53 +0500

    sign out code added

--- app/src/main/java/com/miti/getvaccinated/HomeActivity.kt
+++ app/src/main/java/com/miti/getvaccinated/HomeActivity.kt
@@ -22,6 +22,12 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.miti.getvaccinated.ui.theme.GetVaccinatedTheme
+import androidx.core.content.ContextCompat.startActivity
+
+import com.google.firebase.auth.FirebaseAuth
+
+
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,6 +102,45 @@ fun MainContent(context: Context) {
                         modifier = Modifier.padding(top = 20.dp),
                         text = "Track",
                         textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+        Row() {
+            Card(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth(0.50f)
+                    .padding(start = 10.dp, end = 5.dp, top = 10.dp, bottom = 10.dp)
+                    .clickable {
+                        FirebaseAuth
+                            .getInstance()
+                            .signOut()
+                        val intent = Intent(context, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        intent.putExtra("EXIT", true)
+                        context.startActivity(intent)
+                    },
+                backgroundColor = Color.White,
+                shape = RoundedCornerShape(10.dp)
+
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        modifier = Modifier
+                            .padding(top = 10.dp)
+                            .width(50.dp)
+                            .height(50.dp),
+                        painter = painterResource(id = R.drawable.ic_baseline_article_24),
+                        contentDescription = null
+                    )
+                    Text(
+                        modifier = Modifier.padding(top = 20.dp),
+                        text = "Sign Out",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold
                     )
