b6c3301399509d94efff5ef79eb0ba57de2239b7
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-05-17 07:51:17 +0500

    setContent changes

--- app/src/main/java/com/miti/getvaccinated/LoginActivity.kt
+++ app/src/main/java/com/miti/getvaccinated/LoginActivity.kt
@@ -2,9 +2,27 @@ package com.miti.getvaccinated
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.miti.getvaccinated.ui.theme.GetVaccinatedTheme
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            GetVaccinatedTheme {
+                Surface(color = Color.White) {
+                    LoginContent()
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun LoginContent() {
+
 }
